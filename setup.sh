cab6968663f829d0006484b195dc312c4d9e1cf6
Kurama <60360593+Kurama250@users.noreply.github.com>
2024-06-01 21:32:13 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# setup.sh by Kurama250
+# Github : https://github.com/Kurama250
+
+apt update && apt upgrade -y
+apt install npm nodejs git -y
+if ! command -v node &> /dev/null; then
+    curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
+    apt-get install -y nodejs -y
+else
+    echo "Node.js is already installed. Skipping installation."
+fi
+git clone https://github.com/Kurama250/Anti_bot.git
+cd Anti_bot/
+npm i
+npm install pm2 -g
